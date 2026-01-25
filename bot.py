c0e27d6910b45f10e9e830c4719da3cf2e62de8e
Alex <ag11012008@gmail.com>
2026-01-26 00:23:07 +0200

    Add venv and requirements. Add timezone-aware /sunset, return raw response to user for now. Other minor tweaks

--- bot.py
+++ bot.py
@@ -6,14 +6,25 @@ import sys
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
+from aiogram.types import BotCommand
 
 import handlers
+
+async def setup_bot_commands(bot: Bot):
+    commands = [
+        BotCommand(command="start", description="Display welcome message, set/reset location for forecasts."),
+        BotCommand(command="help", description="Get help"),
+        BotCommand(command="sunset", description="Return the forecast for the next sunset"),
+        BotCommand(command="sunrise", description="Return the forecast for the next sunrise")
+    ]
+
+    await bot.set_my_commands(commands)
+
 
 async def main() -> None:
     # Read bot token
     with open("bot_token.txt", "r") as f:
-        TOKEN = f.read().strip()
-
+        BOT_TOKEN = f.read().strip()
 
     # Initialize the dispatcher and attach our router to it
     dp = Dispatcher()
@@ -25,15 +36,18 @@ async def main() -> None:
             CREATE TABLE IF NOT EXISTS Users (
                 chat_id INTEGER PRIMARY KEY,
                 username TEXT,
-                lat REAL,
-                lon REAL
+                lat REAL NOT NULL CHECK (lat >= -55.0 AND lat <= 70.0),
+                lon REAL NOT NULL CHECK (lon >= -180.0 AND lon <= 180.0)
             )
         """)
         await db.commit() 
 
     # Initialize the bot and start polling
-    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-    await dp.start_polling(bot)
+    bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+
+    dp.startup.register(setup_bot_commands)
+    await dp.start_polling(bot, skip_updates=True, on_startup=setup_bot_commands)
 
 
 if __name__ == "__main__":
--- handlers.py
+++ handlers.py
@@ -1,7 +1,14 @@
 import aiosqlite
+import aiohttp
+
+import timezonefinder
+import zoneinfo
+
+import json
+import datetime
 
 from aiogram import Router, F
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
@@ -12,6 +19,7 @@ router = Router()
 # Use FSM for the registration sequence
 class Registration(StatesGroup):
     waiting_for_location = State()
+
 
 @router.message(CommandStart())
 async def start(message: Message, state: FSMContext):
@@ -25,14 +33,16 @@ async def start(message: Message, state: FSMContext):
     await message.answer("Hello! This bot can fetch sunset/sunrise quality predictions from " 
         "sunsethue.com for your location. Send a location to begin (press button to share your "
         "location or drop a pin manually)", reply_markup=kb)
+    
     # Switch user to the waiting state
     await state.set_state(Registration.waiting_for_location)
+
 
 @router.message(Registration.waiting_for_location, F.location)
 async def handle_location(message: Message, state: FSMContext):
     lat = message.location.latitude
     lon = message.location.longitude
-    user_id = message.from_user.id
+    chat_id = message.from_user.id
     username = message.from_user.username
 
     # Save to database using UPSERT
@@ -44,7 +54,7 @@ async def handle_location(message: Message, state: FSMContext):
                 username = excluded.username,
                 lat = excluded.lat,
                 lon = excluded.lon
-        """, (user_id, username, lat, lon))
+        """, (chat_id, username, lat, lon))
         await db.commit()
 
     await message.answer(f"Saved! Location: {lat}, {lon}", reply_markup=ReplyKeyboardRemove())
@@ -55,3 +65,35 @@ async def handle_location(message: Message, state: FSMContext):
 @router.message(Registration.waiting_for_location)
 async def invalid_location(message: Message):
     await message.answer("That wasn't a location. Please use the button or drop a pin.")
+
+
+@router.message(Command("sunset"))
+async def sunset(message: Message):
+    # Get the user's latitude/longitude.
+    async with aiosqlite.connect("users.sqlite") as db:
+        chat_id = message.chat.id
+        cursor = await db.execute(f"SELECT lat, lon FROM Users WHERE chat_id = ?", (chat_id, )) 
+        coordinates = await cursor.fetchone()
+    
+    if not coordinates:
+        await message.answer("It seems you haven't registered yet. Register using the /start command.")
+
+    # Round to 2 decimal places as the API doesn't offer more precision
+    lat = round(coordinates[0], 2)
+    lon = round(coordinates[1], 2)
+
+    user_timezone_name = timezonefinder.timezone_at(lat=lat, lng=lon)
+    user_datetime = datetime.datetime.now(zoneinfo.ZoneInfo(user_timezone_name))
+
+    utc_datetime = user_datetime.astimezone(zoneinfo.ZoneInfo("UTC"))
+    utc_date = utc_datetime.date()
+    
+    with open("api_key.txt", "r") as f:
+        API_KEY = f.read()
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f"https://api.sunsethue.com/event?key={API_KEY}&latitude={lat}&longitude={lon}&date={utc_date}&type=sunset") as response:
+            await message.answer(str(response.status))
+            data = await response.json()
+            await message.answer(json.dumps(data, indent=2))
+
