ce1acff0330f0de3d33db1a42b87e6d07c55173e
Alex <ag11012008@gmail.com>
2026-04-08 00:08:14 +0300

    Switch to a .env file for api key and bot token

--- bot.py
+++ bot.py
@@ -1,35 +1,44 @@
 import asyncio
-import aiosqlite
 import logging
+import os
 import sys
-import aiohttp
 
+import aiohttp
+import aiosqlite
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.types import BotCommand
+from dotenv import load_dotenv
 
 import handlers
 
+
 async def setup_bot_commands(bot: Bot):
     commands = [
-        BotCommand(command="start", description="Display welcome message, ask to send location"),
+        BotCommand(
+            command="start", description="Display welcome message, ask to send location"
+        ),
         BotCommand(command="sunset", description="Sunset forecast"),
         BotCommand(command="sunrise", description="Sunrise forecast"),
-        BotCommand(command="help", description="Display help message")
+        BotCommand(command="help", description="Display help message"),
     ]
 
     await bot.set_my_commands(commands)
 
 
 async def main() -> None:
+    load_dotenv()
+
     # Read bot token
-    with open("bot_token.txt", "r") as f:
-        BOT_TOKEN = f.read().strip()
-    
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
     # Read API key
-    with open("api_key.txt", "r") as f:
-        API_KEY = f.read()
+    API_KEY = os.getenv("API_KEY")
+    if not BOT_TOKEN or not API_KEY:
+        logging.error(
+            "Failed reading one or more required environment variables. Please set BOT_TOKEN and API_KEY in the .env file."
+        )
+        return
 
     # Ensure the database exists
     async with aiosqlite.connect("users.sqlite") as db:
@@ -49,8 +58,8 @@ async def main() -> None:
                 data["db"] = db
                 data["session"] = session
                 return await handler(event, data)
-            
-            # Initialize the dispatcher and attach our router to it. We only pass the API key 
+
+            # Initialize the dispatcher and attach our router to it. We only pass the API key
             # into the dispatcher as we don't need the bot token after we initialize the bot
             dp = Dispatcher(API_KEY=API_KEY)
             dp.include_routers(handlers.router)
@@ -59,10 +68,14 @@ async def main() -> None:
             dp.update.outer_middleware.register(dependencies_middleware)
 
             # Initialize the bot and start polling
-            bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+            bot = Bot(
+                token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+            )
 
             dp.startup.register(setup_bot_commands)
-            await dp.start_polling(bot, skip_updates=True, on_startup=setup_bot_commands)
+            await dp.start_polling(
+                bot, skip_updates=True, on_startup=setup_bot_commands
+            )
 
 
 if __name__ == "__main__":
